8f8b31a70f87d12954cb1ddb14f4ccbc9e860335
glimlngn <glimlingan@gmail.com>
2025-06-06 19:47:33 +0800

    upskilled!

---
+++ Day 30 - Better Password Manager/main.py
@@ -0,0 +1,100 @@
+from tkinter import * # type: ignore
+from tkinter import messagebox
+import random
+import pyperclip
+import json
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_letters = [random.choice(letters) for _ in range(nr_letters)]
+    password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+    password_list = password_letters + password_symbols + password_numbers
+
+    random.shuffle(password_list)
+    password = "".join(password_list)
+    password_entry.delete(0, END)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+    # print(f"Your password is: {password}")
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+def add_data():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
+
+    if len(website) == 0 or len(email) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Warning ⚠️", message="Please don't leave any fields empty!")
+    else:
+        is_okay = messagebox.askokcancel(title=website, message=f"These are the details entered:\n\nWebsite: {website}"
+                                         f"\nEmail: {email}\nPassword: {password}\n\nIs it okay to save?")
+        if is_okay:
+            try:
+                with open("data.json", "r") as file:
+                    data = json.load(file)    # Reading old data
+                    data.update(new_data)    # Updating/adding old data with new data
+            except FileNotFoundError:
+                print("'data.json' not found. Creating file...")
+                with open("data.json", "w") as file:
+                    json.dump(new_data, file, indent=4)
+            else:
+                with open("data.json", "w") as file:
+                    json.dump(data, file, indent=4)    # Saving the updated data
+            finally:
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
+                website_entry.focus()
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+logo = PhotoImage(file="logo.png")
+canvas = Canvas(width=200, height=200, highlightthickness=0)
+canvas.create_image(100, 100, image=logo)
+canvas.grid(row=0, column=1)
+
+# Labels
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+
+# Entries
+website_entry = Entry()
+website_entry.grid(row=1, column=1, columnspan=2, sticky="EW")
+website_entry.focus()
+email_entry = Entry()
+email_entry.grid(row=2, column=1, columnspan=2, sticky="EW")
+email_entry.insert(0, "glimlingan@gmail.com")
+password_entry = Entry()
+password_entry.grid(row=3, column=1, sticky="EW")
+
+# Buttons
+gen_password_button = Button(text="Generate Password", command=generate_password)
+gen_password_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=36, command=add_data)
+add_button.grid(row=4, column=1, columnspan=2, sticky="EW")
+
+window.mainloop()
