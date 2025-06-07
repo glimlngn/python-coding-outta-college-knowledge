ff841629ce6b78a8181166b83a709c74c0b05d47
glimlngn <glimlingan@gmail.com>
2025-06-07 17:23:53 +0800

    removed finished todos

--- Day 26 - List Comprehension/main.py
+++ Day 26 - List Comprehension/main.py
@@ -3,7 +3,6 @@ import pandas as pd
 df = pd.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row["letter"]:row["code"] for (index, row) in df.iterrows()}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 name = input("Enter a word: ")
 
 name_letters = [letter for letter in name]
