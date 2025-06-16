f8125b014d195847e9a0c8fe7234bccf2c351c9f
glimlngn <glimlingan@gmail.com>
2025-06-16 23:08:22 +0800

    feat: gui shows first question from data, also started using conventional commits ;)

--- Day 34 - GUI Quiz App/ui.py
+++ Day 34 - GUI Quiz App/ui.py
@@ -1,8 +1,11 @@
 from tkinter import * # type: ignore
+from quiz_brain import QuizBrain
+
 THEME_COLOR = "#375362"
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(bg=THEME_COLOR, padx=20, pady=20)
@@ -16,8 +19,9 @@ class QuizInterface:
         # Textbox
         sample_text = "Amazon acquired Twitch in August 2014 blah blah blah."
         self.canvas = Canvas(width=300, height=250, bg="white", highlightthickness=0)
-        self.text_canvas = self.canvas.create_text(150, 125, text=sample_text, 
+        self.question_text = self.canvas.create_text(150, 125, text=sample_text, 
                             fill=THEME_COLOR, width=300, justify="center", font=("Arial", 16, "italic"))
+        self.get_next_question()
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         # Buttons
@@ -29,3 +33,7 @@ class QuizInterface:
         self.false_button.grid(row=2, column=1)
 
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
